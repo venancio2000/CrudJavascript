a2bc2fa59af23969a5f62cdafdf0c304ac4b7729
erica <ericakariny@gmail.com>
2021-06-29 01:02:25 -0300

    inicio

---
+++ js/principal.js
@@ -0,0 +1,31 @@
+var  titulo = document.querySelector(".titulo");
+			titulo.textContent = "Erica Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var tdAltura = paciente.querySelector(".info-altura");
+var tdImc = paciente.querySelector(".info-imc")
+
+var altura = tdAltura.textContent;
+var peso = tdPeso.textContent;
+
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!";
+    }
+    
+    if (altura <= 0 || altura >= 3.00){
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida!";
+    }
+    
+    if (alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc;
+    }
